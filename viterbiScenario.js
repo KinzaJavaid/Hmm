2fc7ea905fa288dfd8989081ddb74fa8b44ff9f4
hiraibrahim <39026606+hiraibrahim@users.noreply.github.com>
2018-06-02 22:25:38 +0500

    Add files via upload

---
+++ viterbiScenario.js
@@ -0,0 +1,30 @@
+var ViterbiAlgorithm = require('hidden-markov-model');
+ 
+var HMM = ViterbiAlgorithm();
+ 
+HMM.setInitialStateVector([0.4, 0.6]); // 1. LCGPA, 2. HCGPA 
+ 
+HMM.setTransitionMatrix([
+    // LCGPA, HCGPA 
+    [0.7, 0.3], //LCGPA
+    [0.6, 0.4]  //HCGPA
+]);
+ 
+HMM.setEmissionMatrix([
+    // HW, Avg, BAvg 
+    [0.2, 0.3, 0.5], // LCGPA
+    [0.6, 0.3, 0.1]  // HCGPA 
+]);
+ 
+  /* What is the probability that the Hidden Markov Model is able to
+   * generate the observed sequence of being HW, Avg, HW, Avg, BAvg?
+   */
+  var alpha = [];
+  var result = HMM.forward([0,1,0,1,2], alpha); // HW, Avg, HW, Avg, BAvg 
+  console.log(result); // 0.03628 
+ 
+  console.log(alpha);
+  /* [ [ 0.3, 0.04000000000000001 ],
+     [ 0.09040000000000001, 0.0342 ],
+     [ 0.007696000000000001, 0.028584000000000002 ] ]
+  */
